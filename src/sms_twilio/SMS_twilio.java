2c0abf802441877bd036dda58e3374e3a1c0de75
OussemaMahjoub <47174218+OussemaMahjoub@users.noreply.github.com>
2019-04-20 03:11:10 +0100

    first

---
+++ src/sms_twilio/SMS_twilio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sms_twilio;
+
+/**
+ * create account twillio and buy number and then verified number first
+ *
+ * @author mahjoub
+ */
+public class SMS_twilio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SMS.SMS(44373307, "hihihihihihih");
+    }
+    
+}
